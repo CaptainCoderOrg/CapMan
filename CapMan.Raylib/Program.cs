344fb7fab3cdfb3c07db31d2ac24bc5422de3960
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-02-03 06:41:41 -0700

    chore: Initialize CapMan project and create basic board data.

---
+++ CapMan.Raylib/Program.cs
@@ -0,0 +1,45 @@
+﻿/*******************************************************************************************
+*
+*   raylib [core] example - Basic window
+*
+*   Welcome to raylib!
+*
+*   To test examples, just press F6 and execute raylib_compile_execute script
+*   Note that compiled executable is placed in the same folder as .c file
+*
+*   You can find all basic examples on C:\raylib\raylib\examples folder or
+*   raylib official webpage: www.raylib.com
+*
+*   Enjoy using raylib. :)
+*
+*   This example has been created using raylib 1.0 (www.raylib.com)
+*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*
+*   Copyright (c) 2013-2016 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+
+using Raylib_cs;
+
+// Initialization
+//--------------------------------------------------------------------------------------
+const int screenWidth = 800;
+const int screenHeight = 450;
+
+Raylib.InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+Raylib.SetWindowMonitor(1);
+Raylib.SetWindowSize(screenWidth, screenHeight);
+Raylib.SetTargetFPS(60);
+
+
+
+// Main game loop
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.RayWhite);
+    Raylib.DrawText("Congrats! You created your first window!", 190, 200, 20, Color.Maroon);
+    Raylib.EndDrawing();
+}
+
+Raylib.CloseWindow();
