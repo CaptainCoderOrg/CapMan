ca669e866bcb09b335712db56b6176eaffe3978b
Derek Gooding <djgooding@live.com>
2024-02-20 14:55:37 -0500

    Update InfoRenderer.cs
    Fix issue #19

--- CapMan.Raylib/InfoRenderer.cs
+++ CapMan.Raylib/InfoRenderer.cs
@@ -2,16 +2,24 @@ namespace CapMan.Raylib;
 using Raylib_cs;
 public class InfoRenderer
 {
-    private readonly int padding = 5;
+    private static readonly int padding = 5;
     private static Font pixelplay = Raylib.LoadFont("assets/fonts/pixelplay.png");
     public static Font PixelPlay => pixelplay;
 
-    public static int BlockedHeight => pixelplay.BaseSize * 2;
+    public static int BlockedHeight => (pixelplay.BaseSize + padding) * 2;
 
     public void Render(Game game, int top, int left)
     {
+        var center = game.Board.Width * BoardRenderer.CellSize / 2;
         int size = pixelplay.BaseSize;
-        Raylib.DrawText("HIGH SCORE", left + padding, top, size, Color.White);
-        Raylib.DrawText($"{game.Score}", left + padding, top + size, size, Color.White);
+
+        string message = "HIGH SCORE";
+        string score = $"{game.Score}";
+
+        var messageOffset = Raylib.MeasureText(message, size) / 2;
+        var scoreOffset = Raylib.MeasureText(score, size) / 2;
+
+        Raylib.DrawText(message, left + center - messageOffset, top + padding, size, Color.White);
+        Raylib.DrawText(score, left + center - scoreOffset, top + size + padding, size, Color.White);
     }
 }
