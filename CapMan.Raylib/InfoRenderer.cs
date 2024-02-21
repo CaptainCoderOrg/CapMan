36048e03470267a22447ef34820e2bb7da10d967
Derek Gooding <djgooding@live.com>
2024-02-20 20:42:47 -0500

    Update InfoRenderer.cs

--- CapMan.Raylib/InfoRenderer.cs
+++ CapMan.Raylib/InfoRenderer.cs
@@ -2,11 +2,11 @@ namespace CapMan.Raylib;
 using Raylib_cs;
 public class InfoRenderer
 {
-    private static readonly int s_padding = 5;
+    private static readonly int Padding = 5;
     private static Font s_pixelplay = Raylib.LoadFont("assets/fonts/pixelplay.png");
     public static Font PixelPlay => s_pixelplay;
 
-    public static int BlockedHeight => (s_pixelplay.BaseSize + s_padding) * 2;
+    public static int BlockedHeight => (s_pixelplay.BaseSize + Padding) * 2;
 
     public void Render(Game game, int top, int left)
     {
@@ -19,7 +19,7 @@ public class InfoRenderer
         int messageOffset = Raylib.MeasureText(message, size) / 2;
         int scoreOffset = Raylib.MeasureText(score, size) / 2;
 
-        Raylib.DrawText(message, left + center - messageOffset, top + s_padding, size, Color.White);
-        Raylib.DrawText(score, left + center - scoreOffset, top + size + s_padding, size, Color.White);
+        Raylib.DrawText(message, left + center - messageOffset, top + Padding, size, Color.White);
+        Raylib.DrawText(score, left + center - scoreOffset, top + size + Padding, size, Color.White);
     }
 }
