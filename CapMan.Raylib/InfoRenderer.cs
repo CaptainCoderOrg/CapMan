c8685dfa6776666b90a51b1386168f8516e97e5e
Derek Gooding <djgooding@live.com>
2024-02-20 20:42:13 -0500

    Update InfoRenderer.cs
    Update for standards

--- CapMan.Raylib/InfoRenderer.cs
+++ CapMan.Raylib/InfoRenderer.cs
@@ -2,24 +2,24 @@ namespace CapMan.Raylib;
 using Raylib_cs;
 public class InfoRenderer
 {
-    private static readonly int padding = 5;
-    private static Font pixelplay = Raylib.LoadFont("assets/fonts/pixelplay.png");
-    public static Font PixelPlay => pixelplay;
+    private static readonly int s_padding = 5;
+    private static Font s_pixelplay = Raylib.LoadFont("assets/fonts/pixelplay.png");
+    public static Font PixelPlay => s_pixelplay;
 
-    public static int BlockedHeight => (pixelplay.BaseSize + padding) * 2;
+    public static int BlockedHeight => (s_pixelplay.BaseSize + s_padding) * 2;
 
     public void Render(Game game, int top, int left)
     {
-        var center = game.Board.Width * BoardRenderer.CellSize / 2;
-        int size = pixelplay.BaseSize;
+        int center = game.Board.Width * BoardRenderer.CellSize / 2;
+        int size = s_pixelplay.BaseSize;
 
         string message = "HIGH SCORE";
         string score = $"{game.Score}";
 
-        var messageOffset = Raylib.MeasureText(message, size) / 2;
-        var scoreOffset = Raylib.MeasureText(score, size) / 2;
+        int messageOffset = Raylib.MeasureText(message, size) / 2;
+        int scoreOffset = Raylib.MeasureText(score, size) / 2;
 
-        Raylib.DrawText(message, left + center - messageOffset, top + padding, size, Color.White);
-        Raylib.DrawText(score, left + center - scoreOffset, top + size + padding, size, Color.White);
+        Raylib.DrawText(message, left + center - messageOffset, top + s_padding, size, Color.White);
+        Raylib.DrawText(score, left + center - scoreOffset, top + size + s_padding, size, Color.White);
     }
 }
