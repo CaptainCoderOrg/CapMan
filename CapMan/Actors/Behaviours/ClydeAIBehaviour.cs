fabd4c8c2194d5ca86d4f60509b73cace111b3de
Captain Coder <3529380+jcollard@users.noreply.github.com>
2024-02-26 11:55:00 -0700

    chore: Fix formatting.

--- CapMan/Actors/Behaviours/ClydeAIBehaviour.cs
+++ CapMan/Actors/Behaviours/ClydeAIBehaviour.cs
@@ -1,6 +1,6 @@
 namespace CapMan;
 
-public class ClydeAIBehaviour(Tile patrol1, Tile patrol2, Tile houseExit) 
+public class ClydeAIBehaviour(Tile patrol1, Tile patrol2, Tile houseExit)
     : TraditionalEnemyAI(patrol1, patrol2, houseExit, new ClydeTargeting(), 7)
 {
 
--- CapMan/Actors/Behaviours/WhimsicalAIBehaviour.cs
+++ CapMan/Actors/Behaviours/WhimsicalAIBehaviour.cs
@@ -1,6 +1,6 @@
 namespace CapMan;
 
-public class WhimsicalAIBehaviour(Tile patrol1, Tile patrol2, Tile houseExit, EnemyActor target) : 
+public class WhimsicalAIBehaviour(Tile patrol1, Tile patrol2, Tile houseExit, EnemyActor target) :
     TraditionalEnemyAI(patrol1, patrol2, houseExit, new WhimsicalTargeting(target), 21)
 {
 
