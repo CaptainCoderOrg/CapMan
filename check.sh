3f87103a0c086108a355c16aed51a0c87c49550e
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-03-02 06:30:19 -0700

    chore: Add test coverage reports.

--- check.sh
+++ check.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 dotnet format style --verify-no-changes --verbosity diagnostic
 dotnet build
-dotnet test
+dotnet test -p:CollectCoverage=true -p:CoverletOutputFormat=cobertura
+reportgenerator -reports:CapMan.Tests/coverage.cobertura.xml  -targetdir:html-coverage-report/
